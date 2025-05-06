afc7fbaf89cf2af4a637de0f4150a61538345756
Mery118 <meriorucva6@gmail.com>
2025-05-06 22:38:57 +0400

    added content to task2

---
+++ 02.js.git-task/task2.js
@@ -0,0 +1,8 @@
+let sampleNews = `As software developers, the natural tendency is to start developing applications based on your own hands-on experience and knowledge right away. However, over time, issues in the application arise, adaptations to changes, and new features happen.`;
+
+// Boşluqların (space-lərin) sayını tapırıq
+let spaceCount = sampleNews.split(' ').length - 1;
+
+// Nəticəni ekrana yazdırırıq
+console.log(`Boşluq sayı: ${spaceCount}`);
+2
